3190a8af1c748ce7b2fd71ce6349483f4369f8c9
Andronik <write@reusable.software>
2023-07-14 16:03:50 +0200

    commit prepare_worker

---
+++ src/prepare_worker.rs
@@ -0,0 +1,6 @@
+pub use polkadot_node_core_pvf_common::decl_worker_main;
+
+polkadot_node_core_pvf_common::decl_worker_main!(
+	"prepare-worker",
+	polkadot_node_core_pvf_prepare_worker::worker_entrypoint
+);
