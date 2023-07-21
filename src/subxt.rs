4f82ce778cbe655907429b49f9cc5a106c972891
ordian <write@reusable.software>
2023-07-21 16:23:54 +0200

    support non-default ExecutorParams (#14)

--- src/subxt.rs
+++ src/subxt.rs
@@ -1,21 +1,15 @@
-use ::subxt::{utils::H256, OnlineClient, PolkadotConfig};
+use ::subxt::{storage::Storage, utils::H256, OnlineClient, PolkadotConfig};
 use anyhow::anyhow;
+use parity_scale_codec::{Decode, Encode};
 use polkadot_parachain::primitives::{Id as ParaId, ValidationCode};
+use polkadot_primitives::ExecutorParams;
 
 #[subxt::subxt(runtime_metadata_path = "assets/kusama_metadata.scale")]
 pub mod polkadot {}
 
 pub async fn fetch_parachain_pvfs(
-    rpc_url: String,
-    at_block: Option<H256>,
+    storage: &Storage<PolkadotConfig, OnlineClient<PolkadotConfig>>,
 ) -> anyhow::Result<Vec<(ParaId, ValidationCode)>> {
-    let api = OnlineClient::<PolkadotConfig>::from_url(rpc_url).await?;
-    let storage = if let Some(block_hash) = at_block {
-        api.storage().at(block_hash)
-    } else {
-        api.storage().at_latest().await?
-    };
-
     let paraids_query = polkadot::storage().paras().parachains();
     let paraids = storage.fetch(&paraids_query).await?;
     let paraids = paraids.ok_or(anyhow!("parachains storage should be initialized"))?;
@@ -42,4 +36,36 @@ pub async fn fetch_parachain_pvfs(
     }
 
     Ok(pvfs)
+}
+
+pub async fn fetch_on_chain_data(
+    rpc_url: String,
+    at_block: Option<H256>,
+) -> anyhow::Result<(Vec<(ParaId, ValidationCode)>, ExecutorParams)> {
+    let api = OnlineClient::<PolkadotConfig>::from_url(rpc_url).await?;
+    let storage = if let Some(block_hash) = at_block {
+        api.storage().at(block_hash)
+    } else {
+        api.storage().at_latest().await?
+    };
+
+    let pvfs = fetch_parachain_pvfs(&storage).await?;
+
+    let session_index = storage
+        .fetch(&polkadot::storage().paras_shared().current_session_index())
+        .await?
+        .ok_or(anyhow!("missing session index"))?;
+
+    let executor_params = storage
+        .fetch(
+            &polkadot::storage()
+                .para_session_info()
+                .session_executor_params(session_index),
+        )
+        .await?
+        .ok_or(anyhow!("missing executor params"))?;
+    let encoded = executor_params.encode();
+    // FIXME: we need to convert types between polkadot and subxt properly.
+    let executor_params = ExecutorParams::decode(&mut encoded.as_slice())?;
+    Ok((pvfs, executor_params))
 }
