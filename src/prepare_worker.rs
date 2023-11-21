88f348c2264cbee204075bbcf645274940ff38b0
Marcin S. <marcin@realemail.net>
2023-11-21 12:51:56 +0100

    cargo fmt

--- src/prepare_worker.rs
+++ src/prepare_worker.rs
@@ -4,5 +4,5 @@ polkadot_node_core_pvf_common::decl_worker_main!(
     "prepare-worker",
     polkadot_node_core_pvf_prepare_worker::worker_entrypoint,
     env!("SUBSTRATE_CLI_IMPL_VERSION"),
-	env!("SUBSTRATE_CLI_COMMIT_HASH"),
+    env!("SUBSTRATE_CLI_COMMIT_HASH"),
 );
